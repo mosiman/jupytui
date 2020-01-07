b2517950878b9d7f89656d278adfa28c382a9b2d
Dillon Chan <dillonchan97@gmail.com>
2020-01-06 22:55:35 -0500

    rework: Clean up for merge into master

--- JupytuiWidgets.py
+++ JupytuiWidgets.py
@@ -1,8 +1,28 @@
-# TODO: refactor into misc, Widgets, States
-
-import logging
-
 import urwid
+
+def handleCodeOutput(outputs):
+    """
+    Takes the outputs of a cell, returns a list of appropriate widgets to be used in a Pile
+    """
+    outwidgets = []
+    for output in outputs:
+        if output["output_type"] == 'stream':
+            lines = output["text"].split('\n')
+            outwidgets.append(urwid.Text(output["text"]))
+        elif output["output_type"] == 'display_data':
+            widg = NiceButton('<< Some display data >>', on_press=None)
+            outwidgets.append(urwid.AttrMap(widg, 'regularText', 'cellFocus'))
+        elif output["output_type"] == 'execute_result':
+            data = output["data"]
+            for datatype in data.keys():
+                if datatype == "text/plain":
+                    outwidgets.append(urwid.Text(data['text/plain']))
+                else:
+                    widg = NiceButton(f'<< Some {datatype} >>', on_press=None)
+                    outwidgets.append(urwid.AttrMap(widg, 'regularText', 'cellFocus'))
+                outwidgets.append(urwid.Text(''))
+        outwidgets.append(urwid.Text(''))
+    return outwidgets
 
 class ButtonLabel(urwid.SelectableIcon):
     '''
@@ -44,124 +64,6 @@ class NiceButton(urwid.Button):
             urwid.connect_signal(self, 'click', on_press, user_data)
 
         self.set_label(label)
-
-
-
-def handleCodeOutput(outputs):
-    """
-    Takes the outputs of a cell, returns a list of appropriate widgets to be used in a Pile
-    """
-    outwidgets = []
-    for output in outputs:
-        if output["output_type"] == 'stream':
-            lines = output["text"].split('\n')
-            outwidgets.append(urwid.Text(output["text"]))
-        elif output["output_type"] == 'display_data':
-            widg = NiceButton('<< Some display data >>', on_press=None)
-            outwidgets.append(urwid.AttrMap(widg, 'regularText', 'cellFocus'))
-        elif output["output_type"] == 'execute_result':
-            data = output["data"]
-            for datatype in data.keys():
-                if datatype == "text/plain":
-                    outwidgets.append(urwid.Text(data['text/plain']))
-                else:
-                    widg = NiceButton(f'<< Some {datatype} >>', on_press=None)
-                    outwidgets.append(urwid.AttrMap(widg, 'regularText', 'cellFocus'))
-                outwidgets.append(urwid.Text(''))
-        outwidgets.append(urwid.Text(''))
-    return outwidgets
-
-class StateBase:
-    def __init__(self, context):
-        self.context = context
-    def keypress(self, size, key):
-        pass
-
-class EditState(StateBase):
-    def __init__(self, context):
-        super().__init__(context)
-        SelectableEdit._selectable = True
-        self.context.cmdbox.edit_text = "(EDIT)"
-        # If focus happens to be on a button, we need to force the focus to be on the source.
-        # TODO this kinda smells tbh, should be accessible more directly from StatefulFrame
-        self.context.listbox.focus._w.focus_position = 0
-    def keypress(self, size, key):
-        logging.debug(f'keypress caught by EditState: {key}')
-        if key in ['esc']:
-            # Switch to Nav state
-            self.context._state = NavState(self.context)
-            return
-        self.context.superkeypress(size, key)
-
-class CmdState(StateBase):
-    def __init__(self, context):
-        super().__init__(context)
-        context.cmdbox.edit_text = ":"
-        context.focus_part = 'footer'
-
-    def keypress(self, size, key):
-        # Avoid all unless it's 'enter'
-        logging.debug(f'key press caught by CmdState: {key}')
-        if key in ['enter']:
-            # TODO: process the command here
-            # ...
-            self.context.focus_part = 'body'
-            self.context._state = NavState(self.context)
-        keyResult = self.context.superkeypress(size, key)
-        if keyResult:
-            return keyResult
-
-class NavState(StateBase):
-    def __init__(self, context):
-        super().__init__(context)
-        # Set edits to be not selectable in Nav mode
-        SelectableEdit._selectable = False
-        self.context.cmdbox.edit_text = "(NAV)"
-    def keypress(self, size, key):
-        logging.debug(f'key press caught by NavState: {key}')
-        if key in ['j', 'k', 'g', 'G']:
-            currentFocusPos = self.context.listbox.focus_position
-            if key == 'j':
-                try:
-                    nextFocusPos = self.context.listbox.body.next_position(currentFocusPos)
-                except IndexError:
-                    nextFocusPos = currentFocusPos
-            if key == 'k':
-                try:
-                    nextFocusPos = self.context.listbox.body.prev_position(currentFocusPos)
-                except IndexError:
-                    nextFocusPos = currentFocusPos
-            if key == 'g':
-                pass
-            if key == 'G':
-                pass
-            self.context.listbox.set_focus(nextFocusPos)
-        if key in ['i', 'a']:
-            # Switch to Edit state
-            self.context._state = EditState(self.context)
-            return
-        if key in [':']:
-            self.context._state = CmdState(self.context)
-        return key
-
-class StatefulFrame(urwid.Frame):
-
-    def __init__(self, body, header=None, footer=None, focus_part='body'):
-        super().__init__(body, header=header, footer=footer, focus_part=focus_part)
-        # initial state
-        self.cmdbox = self.footer[1]
-        self.listbox = self.body
-        self._state = NavState(self)
-
-    def keypress(self, size, key):
-        keyResult = self._state.keypress(size, key)
-        if keyResult:
-            return keyResult
-
-    def superkeypress(self, size, key):
-        keyResult = super().keypress(size, key)
-        if keyResult:
-            return keyResult
 
 class SelectableEdit(urwid.Edit):
     """
--- jupytui.py
+++ jupytui.py
@@ -1,285 +1,67 @@
-import urwid 
-import os
+import urwid
+
+# Monkeypatch: issue 386
+
+import patch_issue_386
+urwid.ListBox = patch_issue_386.ListBox
+
+import nbformat
+from JupytuiWidgets import Cell, NotebookWalker, PopUpDialog, SelectableEdit
+from State import StatefulFrame
+
 import logging
-# from pixcat import Image
-
-
-import json
-
-from NbkUrwid import NbkCellLineBox, NbkCellWalker, NbkListBox, \
-                     NbkEditBox, \
-                     NbkCellRaw, NbkCellMkdn, NbkCellCode
-
-logging.basicConfig(filename='jupytui.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-
-
-def show_or_exit(key):
-    # print(f"unhandled key: {key}")
-    # img = Image("/home/mosiman/test.png")
-    # img.show(x=50, y=50)
-    # modalLW.set_focus(modalLW.focus + 1)
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
+logging.basicConfig(filename='rework.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 palette = [
-        ('banner', 'black', 'light gray'),
-        ('streak', 'black', 'dark red'),
+        ('fnamebox', 'black', 'white'),
         ('bg', 'black', 'dark blue'),
         ('cellFocus', 'light green', ''),
-        ('foot', 'yellow', 'dark gray')
+        ('foot', 'yellow', 'dark gray'),
+        ('regularText', '', ''),         # so attrmap(linebox) doesn't assign to text
+        ('regularLineBox', '', ''),         # so attrmap(linebox) doesn't assign to text
         ]
 
+# read a notebook
+fname = 'census.ipynb'
+nbk = nbformat.read(fname, nbformat.NO_CONVERT)
+
+####### SIGNALS #########
+
+def undoOverlayMessage():
+    loop.widget = loop.widget.bottom_w
+
+def debug_input(key):
+    logging.debug(f"unhandled key: {key}")
+    if key == 'f4':
+        logging.debug('f4 pressed')
+        pop_up = PopUpDialog("foobar!")
+        overlay = urwid.Overlay(pop_up, loop.widget, align='center', width=('relative', 80), valign='middle', height=('relative', 80))
+        loop.widget = overlay
+        urwid.connect_signal(pop_up, 'close',
+                lambda button: undoOverlayMessage())
+    if key == 'f1':
+        logging.debug(f'cell[2]: {loop.widget.body.body.cells[2]}')
+    if key == 'f2':
+        logging.debug(f'f2 pressed')
+        logging.debug(f'Setting cell[2] editbox to be not selectable')
+        SelectableEdit._selectable = False
+    if key == 'f3':
+        logging.debug(f'f3 pressed')
+        logging.debug(f'Setting editboxes to be selectable again')
+        SelectableEdit._selectable = True
 
 
-def add_editbox(key):
-    if key in ('q', 'Q'):
-        # raise urwid.ExitMainLoop()
-        nbkLW.append(urwid.LineBox(urwid.Text("sup2")))
-    return True
+nbkWalker = NotebookWalker(nbk)
+listcell = urwid.ListBox(nbkWalker)
 
-def handleCodeOutput(outputs):
-    outstr = ""
-    for output in outputs:
-        if output["output_type"] == 'stream':
-            outstr += ' > ' + ' > '.join(output["text"])
-        elif output["output_type"] == 'display_data':
-            outstr += ' > << Some display data >>'
-        elif output["output_type"] == 'execute_result':
-            data = output["data"]
-            for datatype in data.keys():
-                if datatype == "text/plain":
-                    outstr += ' > ' + ' > '.join(data['text/plain'])
-                else:
-                    outstr += f' > << Some {datatype} >> '
-                outstr += '\n'
-        outstr += '\n'
-    return outstr
+cmdbox = urwid.Edit(edit_text="(NAV)")
+fnamebox = urwid.AttrMap(urwid.Text(fname), 'fnamebox')
+footer = urwid.Pile([fnamebox, cmdbox])
 
-def chooseNbkCell(cell):
-    if 'execution_count' in cell:
-        runNum = cell['execution_count']
-        logging.debug(f"execution count found: {cell['execution_count']}")
-    else:
-        runNum = ''
+#frame = urwid.Frame(listcell, footer=footer)
 
-    if cell["cell_type"] == 'markdown':
-        return NbkCellMkdn(''.join(cell["source"]), runNum=runNum)
-    elif cell["cell_type"] == 'code':
-        # process the output 
-        out = handleCodeOutput(cell["outputs"])
-        return NbkCellCode(''.join(cell["source"]), out, runNum=runNum)
-    elif cell["cell_type"] == 'raw':
-        return NbkCellRaw(''.join(cell["source"]), runNum=runNum)
+frame = StatefulFrame(listcell, footer=footer)
 
+loop = urwid.MainLoop(frame, palette, unhandled_input=debug_input, pop_ups=True)
 
-class StatusBar(urwid.Edit):
-    def __init__(self, caption=u"", edit_text=u"", multiline=False,
-            align='left', wrap='space', allow_tab=False,
-            edit_pos=None, layout=None, mask=None, selectable=False):
-        super().__init__(caption=caption, edit_text=edit_text, multiline=multiline,
-            align=align, wrap=wrap, allow_tab=allow_tab,
-            edit_pos=edit_pos, layout=layout, mask=mask)
-
-        self._selectable = selectable
-        
-    def selectable(self):
-        return self._selectable
-    def set_selectable(self,b):
-        self._selectable = b
-
-    def keypress(self, size, key):
-
-        if key == 'enter':
-            logging.debug(f"Command to be sent: {self.edit_text}")
-            # urwid.emit_signal(self, "modeChange", "NAV")
-            urwid.emit_signal(self, "commandSend", self.edit_text.split(':')[1])
-        elif key == 'backspace':
-            if self.edit_pos == 1:
-                urwid.emit_signal(self, "modeChange", "NAV")
-            else:
-                super().keypress(size,key)
-        else:
-            super().keypress(size, key)
-
-
-class ModalManager:
-    def __init__(self, mode='NAV'):
-        self.mode = mode 
-
-    def switch_mode(self, mode):
-        self.mode = mode
-
-class ModalFrame(urwid.Frame):
-    def __init__(self, body, modeman, header=None, footer=None, focus_part='body'):
-        super().__init__(body, header=header, footer=footer, focus_part=focus_part)
-        self.modeman = modeman 
-
-    def changeMode(self, mode):
-        prevMode = self.modeman.mode 
-
-        logging.debug(f"chageMode signal caught")
-        logging.debug(f"prevmode: {prevMode}")
-        logging.debug(f"mode: {mode}")
-
-        if prevMode == 'NAV' and mode == 'COMMAND':
-            logging.debug("prevmode NAV, mode COMMAND")
-            # set focus to footer 
-            self.footer.base_widget.set_selectable(True)
-            self.footer.base_widget.set_edit_text(':')
-            self.modeman.mode = mode
-            self.focus_position = 'footer'
-            pass
-        elif prevMode == 'NAV' and mode == 'CELL':
-            # focus should already be on body 
-            self.modeman.mode = mode
-            self.footer.base_widget.set_edit_text('Vi (CELL)')
-            pass 
-        elif prevMode == 'CELL' and mode == 'NAV':
-            logging.debug("prevmode CELL, mode NAV")
-            # focus should already be on body
-            self.modeman.mode = mode
-            self.footer.base_widget.set_edit_text('Vi (NAV)')
-            pass
-        elif prevMode == 'COMMAND' and mode == 'NAV':
-            logging.debug("prevmode COMMAND, mode NAV")
-            # set focus to body 
-            self.footer.base_widget.set_selectable(False)
-            self.footer.base_widget.set_edit_text('Vi (NAV)')
-            self.modeman.mode = mode
-            self.focus_position = 'body'
-        self.modeman.mode == mode
-
-    def keypress(self, size, key):
-        """
-        Going to manage how keypresses get filtered out to appropriate components.
-        Let the children manage the switches.
-        """
-        logging.debug(f"keypress, modal frame with key {key}")
-        
-        #should use self.focus_position = 'body' instead of set_focus
-
-        # if mode = 'NAV', should go to listbox 
-        # if self.modeman == 'NAV':
-        #     self.set_focus('body')
-        # # if mode = 'CELL', should go to listbox 
-        # if self.modeman == 'CELL':
-        #     self.set_focus('body')
-        # # if mode = 'COMMAND', should go to footer
-        # if self.modeman == 'COMMAND':
-        #     self.set_focus('footer')
-        return super().keypress(size, key)
-
-
-def modalSignalCatcher():
-    logging.debug("model signal catcher called")
-    if modeman.mode == 'NAV':
-        modalFooter.base_widget.set_caption('Vi (foobar)')
-
-def commandCatcher(cmd):
-    """
-    A test function to see how things work
-    """
-
-    logging.debug(f"command catcher: {cmd}")
-
-    def returnToNav(arg):
-        logging.debug(f"buttonOnClick arg: {arg}")
-        loop.widget = view
-        loop.widget.changeMode('NAV')
-
-    # 'single word' functions
-    if cmd in ['q', 'exit']:
-        raise urwid.ExitMainLoop
-    elif cmd in ['w']:
-        okayBox = urwid.LineBox(urwid.Pile([urwid.Text("A placeholder for the save function"), urwid.Button('Okay', on_press=returnToNav)]))
-        loop.widget = urwid.Overlay(okayBox, view, align='center', width=('relative', 80), valign='middle', height='pack')
-        return
-    elif cmd in ['h', 'help']:
-        okayBox = urwid.LineBox(urwid.Pile([urwid.Text("A placeholder for the help function"), urwid.Button('Okay', on_press=returnToNav)]))
-        loop.widget = urwid.Overlay(okayBox, view, align='center', width=('relative', 80), valign='middle', height='pack')
-    elif cmd in ['delete']:
-        logging.debug(f"delete")
-        loop.widget.body.body.pop(loop.widget.body.body.focus)
-        loop.widget.changeMode('NAV')
-        return
-    elif cmd in ['deleteall']:
-        loop.widget.body.body = []
-        loop.widget.changeMode('NAV')
-        return
-    elif cmd in ['addtest']:
-        focus = loop.widget.body.body.focus 
-        curCell = loop.widget.body.body[focus]
-        # insert same thing 
-        loop.widget.body.body.insert(focus, curCell)
-        loop.widget.changeMode('NAV')
-        loop.screen.clear()
-        return
-
-
-    # 'two word' functions
-    fname, arg = cmd.split(' ')
-
-    if fname in ['o', 'open']:
-        # try to find the file 
-        fileExists = os.path.exists(arg)
-        if fileExists:
-            # read file, do the thing
-            #nbkListBox.body = NbkCellWalker([])
-            loop.widget.body.body = []
-            with open(arg, 'r') as f:
-                ipynb = json.load(f)
-            
-            cells = ipynb["cells"]
-
-            for cell in cells:
-                #nbkListBox.body.append(chooseNbkCell(cell))
-                loop.widget.body.body.append(chooseNbkCell(cell))
-            # view = ModalFrame(nbkListBox, modeman, footer=modalFooter)
-            # loop.widget = view
-            view.changeMode('NAV')
-
-        else:
-            okayBox = urwid.LineBox(urwid.Pile([urwid.Text("File not found!"), urwid.Button('Okay', on_press=returnToNav)]))
-            loop.widget = urwid.Overlay(okayBox, view, align='center', width=('relative', 80), valign='middle', height='pack')
-
-        return
-
-    
-    
-
-    okayBox = urwid.LineBox(urwid.Pile([urwid.Text(f"uncaught command {cmd}"), urwid.Button('Okay', on_press=returnToNav)]))
-    loop.widget = urwid.Overlay(okayBox, view, align='center', width=('relative', 80), valign='middle', height='pack')
-
-# register signals and stuff 
-urwid.register_signal(NbkListBox, ["nbklist insert mode", "modeChange"])
-urwid.register_signal(StatusBar, ["modeChange", "commandSend"])
-
-
-modeman = ModalManager()
-nbkLW = NbkCellWalker([])
-#nbkListBox = urwid.ListBox(nbkLW)
-statusBar = StatusBar(edit_text = 'Vi (NAV)', selectable=True)
-modalFooter = urwid.AttrMap(statusBar, 'foot')
-nbkListBox = NbkListBox(nbkLW, modeman)
-#view = urwid.Frame(nbkListBox, footer=modalFooter)
-view = ModalFrame(nbkListBox, modeman, footer=modalFooter)
-
-urwid.connect_signal(nbkListBox, "modeChange", view.changeMode)
-urwid.connect_signal(statusBar, "modeChange", view.changeMode)
-urwid.connect_signal(statusBar, "commandSend", commandCatcher)
-
-with open('census.ipynb', 'r') as f:
-    ipynb = json.load(f)
-
-cells = ipynb["cells"]
-
-testcell = chooseNbkCell(cells[0])
-
-for cell in cells:
-    nbkLW.append(chooseNbkCell(cell))
-
-
-loop = urwid.MainLoop(view, palette, unhandled_input = show_or_exit)
-# sketchy shit to put the select attr when loading up.
-view.body.set_focus(view.body.focus_position)
 loop.run()
